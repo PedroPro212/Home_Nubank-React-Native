c45806caf795b8c820d3a3eb57584d90147a05d4
Pedro Oliveira <pedrogamer212004@gmail.com>
2024-10-07 19:33:22 -0300

    style
    Header

--- src/modules/home/screens/Home.tsx
+++ src/modules/home/screens/Home.tsx
@@ -1,12 +1,35 @@
 import React from "react";
-import { Text, StyleSheet, SafeAreaView, View } from "react-native";
+import { Text, Image, StyleSheet, SafeAreaView, View } from "react-native";
 
 const Home = () => {
     return (
         <SafeAreaView style={styles.container}>
             <View>
                 <View style={styles.header}>
-                    
+                    <View style={{flexDirection: 'row', justifyContent: 'space-between', padding: 20}}>
+                        <View style={styles.header_img_user_backg}>
+                            <Image
+                                style={styles.header_img_user}
+                                source={require('../imgs/user.png')}
+                            />
+                        </View>
+                        <View style={{flexDirection: 'row', gap: 15}}>
+                            <Image
+                                style={styles.header_img_opcoes}
+                                source={require('../imgs/olho2.png')}
+                            />
+                            <Image
+                                style={styles.header_img_opcoes}
+                                source={require('../imgs/interrogacao2.png')}
+                            />
+                            <Image
+                                style={styles.header_img_opcoes}
+                                source={require('../imgs/email2.png')}
+                            />
+                        </View>
+                    </View>
+
+                    <Text style={styles.header_nome}>Olá, Pedro</Text>
                 </View>
             </View>
         </SafeAreaView>
@@ -15,12 +38,38 @@ const Home = () => {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        backgroundColor: '#F5F5F5'
     },
     header: {
         width: '100%',
-        height: 130,
+        height: '40%',
         backgroundColor: '#8A05BE'
+    },
+    header_img_user:{
+        width: 35,
+        height: 35,
+        alignContent: 'center',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    header_img_user_backg: {
+        width: 55,
+        height: 55,
+        backgroundColor: 'rgba(255, 255, 255, 0.3)',
+        borderRadius: 50,
+        alignContent: 'center',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    header_img_opcoes: {
+        width: 30,
+        height: 30
+    },
+    header_nome: {
+        color: '#FFF',
+        fontSize: 20,
+        paddingLeft: 20
     }
 })
 
