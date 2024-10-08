357af34a92dd19823ed7f1298050e753a285c32c
Pedro Oliveira <pedrogamer212004@gmail.com>
2024-10-07 22:08:29 -0300

    feat
    Carrossel opções

--- src/modules/home/screens/Home.tsx
+++ src/modules/home/screens/Home.tsx
@@ -1,10 +1,46 @@
 import React from "react";
-import { Text, Image, StyleSheet, SafeAreaView, View } from "react-native";
+import { Text, Image, StyleSheet, SafeAreaView, View, FlatList, StatusBar } from "react-native";
+
+const DATA = [
+    {
+        id: 1,
+        title: 'Pix',
+    },
+    {
+        id: 2,
+        title: 'Pagar',
+    },
+    {
+        id: 3,
+        title: 'Transferir',
+    },
+    {
+        id: 4,
+        title: 'Depositar',
+    },
+    {
+        id: 5,
+        title: 'Pegar emprestado',
+    },
+    {
+        id: 6,
+        title: 'Recarga celular'
+    }
+];
+
+type ItemProps = {title: string};
+
+const Item = ({title}: ItemProps) => (
+    <View style={styles.item}>
+        <Text style={styles.title}>{title}</Text>
+    </View>
+);
 
 const Home = () => {
     return (
         <SafeAreaView style={styles.container}>
             <View>
+
                 <View style={styles.header}>
                     <View style={{flexDirection: 'row', justifyContent: 'space-between', padding: 20}}>
                         <View style={styles.header_img_user_backg}>
@@ -28,9 +64,30 @@ const Home = () => {
                             />
                         </View>
                     </View>
-
                     <Text style={styles.header_nome}>Olá, Pedro</Text>
                 </View>
+
+                <View style={{padding: 20}}>
+                    <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+                        <Text style={{fontSize: 24, color: 'black'}}>Conta</Text>
+                        <Image
+                            style={{width: 20, height: 35}}
+                            source={require('../imgs/proximo.png')}
+                        />
+                    </View>
+                    <Text style={{fontSize: 28, color: 'black', fontWeight: 'bold'}}>R$ 674,65</Text>
+                </View>
+
+                <View /*style={styles.container2}*/>
+                    <FlatList 
+                        data={DATA}
+                        renderItem={({item}) => <Item title={item.title} />}
+                        keyExtractor={item => item.id.toString()}
+                        horizontal={true}   // Rolar na horizontal
+                        showsHorizontalScrollIndicator={false}  // Oculta barra de rolagem
+                    />
+                </View>
+
             </View>
         </SafeAreaView>
     )
@@ -43,7 +100,7 @@ const styles = StyleSheet.create({
     },
     header: {
         width: '100%',
-        height: '40%',
+        height: '35%',
         backgroundColor: '#8A05BE'
     },
     header_img_user:{
@@ -70,7 +127,21 @@ const styles = StyleSheet.create({
         color: '#FFF',
         fontSize: 20,
         paddingLeft: 20
-    }
+    },
+    item: {
+        backgroundColor: '#f9c2ff',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+        borderRadius: 50
+    },
+    title: {
+        fontSize: 32,
+    },
+    container2: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight || 0,
+    },
 })
 
 export default Home;
